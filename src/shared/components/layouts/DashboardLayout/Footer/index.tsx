5b0e403f1f19c23cd158332a575825f2200b4013
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-04-06 09:27:58 -0400

    style: footer responsive

--- src/shared/components/layouts/DashboardLayout/Footer/index.tsx
+++ src/shared/components/layouts/DashboardLayout/Footer/index.tsx
@@ -4,12 +4,16 @@ import { Wrapper } from "../../../elements";
 
 export const DashboardLayoutFooter = () => {
   return (
-    <Wrapper as="footer" className="flex items-center justify-between">
-      <Box className="flex items-center justify-between">
+    <Wrapper as="footer">
+      <Box className="flex flex-col-reverse md:flex-row items-center justify-between">
         <Text className="text-sm text-textGray-light">
-          2023 © Judit.io · Todos os direitos reservados
+          2023 © Judit.io
+          <span className="hidden md:block">
+            {" "}
+            · Todos os direitos reservados
+          </span>
         </Text>
-        <Text className="text-sm text-textGray-dark">
+        <Text className="text-sm text-textGray-dark text-center mb-2 md:mb-0">
           Termos de uso · Política de privacidade · Cookies
         </Text>
       </Box>
