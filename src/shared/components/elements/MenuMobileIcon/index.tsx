635dc0b24556ba6253050f0bf222850d5ac75227
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-04-06 09:41:36 -0400

    style: adjusts in menu mobile icon

--- src/shared/components/elements/MenuMobileIcon/index.tsx
+++ src/shared/components/elements/MenuMobileIcon/index.tsx
@@ -1,11 +1,23 @@
 import { Squash } from "hamburger-react";
 import { Dispatch, SetStateAction } from "react";
+import Icon from "../Icon";
 
 interface Props {
   isOpen: boolean;
   onChange: Dispatch<SetStateAction<boolean>>;
 }
 
-export const MenuMobileIcon = ({ isOpen, onChange }: Props) => {
+/* export const MenuMobileIcon = ({ isOpen, onChange }: Props) => {
   return <Squash toggled={isOpen} toggle={onChange} duration={0.2} />;
+}; */
+
+export const MenuMobileIcon = ({ isOpen, onChange }: Props) => {
+  return (
+    <button
+      className="h-full flex items-center"
+      onClick={() => onChange(!isOpen)}
+    >
+      <Icon name={isOpen ? "List" : "X"} size={32} />
+    </button>
+  );
 };
