ad40c7fcdcffb21e4a0bb97774bee246a87dc9fa
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-05-22 14:14:42 -0400

    adjusts home banner

--- src/components/layouts/HomePage/Banner/index.tsx
+++ src/components/layouts/HomePage/Banner/index.tsx
@@ -4,34 +4,36 @@ import { ButtonPrimary } from 'components/elements/Button'
 
 export const Banner = () => {
   return (
-    <div className="relative">
-      <Section className="h-screen flex">
-        <div className="flex items-center h-full">
-          <div className="w-full md:w-1/2 text-center md:text-left">
-            <h1 className="text-white uppercase font-medium text-5xl md:text-6xl pb-5">
-              A melhor solução para{' '}
-              <span className="text-primary-100">last-mile</span>
-            </h1>
-            <p className="font-normal text-base pb-8">
-              Nossas soluções são a chave para aumentar a eficiência da sua
-              operação. Acelere processos, reduza custos e ganhe mais
-              produtividade com uma plataforma completa de otimização de rotas e
-              gestão de entregas e coletas.
-            </p>
-            <div className="lg:w-56">
-              <ButtonPrimary href="/">Quero uma demonstração</ButtonPrimary>
-            </div>
+    <Section className="h-screen flex">
+      <div className="flex h-full relative test min-h-[640px]">
+        <div className="md:max-w-lg md:mx-auto lg:mx-0 flex flex-col justify-center w-full h-full lg:w-1/2 text-center lg:text-left">
+          <h1 className="text-white uppercase font-medium text-5xl lg:text-6xl pb-5">
+            A melhor solução para{' '}
+            <span className="text-primary-100">last-mile</span>
+          </h1>
+          <p className="font-normal text-base pb-8">
+            Nossas soluções são a chave para aumentar a eficiência da sua
+            operação. Acelere processos, reduza custos e ganhe mais
+            produtividade com uma plataforma completa de otimização de rotas e
+            gestão de entregas e coletas.
+          </p>
+          <div className="lg:w-56">
+            <ButtonPrimary href="/">Quero uma demonstração</ButtonPrimary>
           </div>
         </div>
-        <div className="h-full absolute top-0 right-0 w-1/2 hidden md:block">
-          <Image
-            style={{ objectFit: 'cover' }}
-            fill
-            src="/images/pictures/home-banner-1.svg"
-            alt="Banner"
-          />
+        <div className="hidden lg:block absolute w-3/4 -right-36 top-0 h-full">
+          <div className="relative h-full">
+            <Image
+              style={{
+                objectFit: 'contain'
+              }}
+              fill
+              src="/images/pictures/home-banner-1.svg"
+              alt="Banner"
+            />
+          </div>
         </div>
-      </Section>
-    </div>
+      </div>
+    </Section>
   )
 }
--- src/components/layouts/WebsiteLayout/index.tsx
+++ src/components/layouts/WebsiteLayout/index.tsx
@@ -10,7 +10,7 @@ export const WebsiteLayout = ({ children }: Props) => {
   return (
     <div className="bg-grayscale-700">
       <Header />
-      <main className="pt-[124px] lg:pt-[88px]">{children}</main>
+      <main /* className="pt-[124px] lg:pt-[88px]" */>{children}</main>
       <Footer />
     </div>
   )
