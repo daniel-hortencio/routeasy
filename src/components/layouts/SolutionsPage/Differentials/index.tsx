a385060b6387b80faa9366305f07670b76bbb068
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-05-23 17:29:23 -0400

    adjusts section diferentials

--- src/components/layouts/SolutionsPage/Differentials/index.tsx
+++ src/components/layouts/SolutionsPage/Differentials/index.tsx
@@ -6,6 +6,8 @@ export const Differentials = () => {
     <Section
       title={<Title>Diferenciais</Title>}
       subtitle="Lorem ipsum dolor sit amet. Ut sint laboriosam ut sapiente rerum aut assumenda voluptates qui beatae quis id Quis cupiditate. Cum veritatis voluptatem hic dolores fuga eum dolorum tenetur est iusto quis. "
-    ></Section>
+    >
+      <p>Conteúdo</p>
+    </Section>
   )
 }
