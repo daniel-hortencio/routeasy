c3d81a38103d71febffdefaf40f4e98167037d94
cheilamelo <cheila.melo@routeasy.com.br>
2023-05-23 15:44:39 -0300

    Differentials component created

---
+++ src/components/layouts/SolutionsPage/Differentials/index.tsx
@@ -0,0 +1,11 @@
+import { Section } from 'components/elements/Section'
+import { Title } from 'components/elements/Texts'
+
+export const Differentials = () => {
+  return (
+    <Section
+      title={<Title>Diferenciais</Title>}
+      subtitle="Lorem ipsum dolor sit amet. Ut sint laboriosam ut sapiente rerum aut assumenda voluptates qui beatae quis id Quis cupiditate. Cum veritatis voluptatem hic dolores fuga eum dolorum tenetur est iusto quis. "
+    ></Section>
+  )
+}
--- src/components/layouts/SolutionsPage/index.tsx
+++ src/components/layouts/SolutionsPage/index.tsx
@@ -3,6 +3,7 @@ import { Segments } from '../WebsiteLayout/Segments'
 import { ContactForm } from './ContactForm'
 import { FeaturedFrame } from './FeaturedFrame'
 import { Integrating } from '../WebsiteLayout/Integrating'
+import { Differentials } from './Differentials'
 
 export const SolutionsPage = () => {
   return (
@@ -13,6 +14,7 @@ export const SolutionsPage = () => {
         text="Lorem ipsum dolor sit amet. Ut sint laboriosam ut sapiente rerum aut assumenda voluptates qui beatae quis id Quis cupiditate. Cum veritatis voluptatem hic dolores fuga eum dolorum tenetur est iusto quis. "
         image="/images/pictures/max-mid.png"
       />
+      <Differentials />
       <Integrating />
       <Segments />
       <ContactForm />
