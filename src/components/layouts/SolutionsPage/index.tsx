aad997cd97dd09fb9081c4f74b312278e9d293fd
cheilamelo <cheila.melo@routeasy.com.br>
2023-05-23 12:22:57 -0300

    Section Featured Frame

--- src/components/layouts/SolutionsPage/index.tsx
+++ src/components/layouts/SolutionsPage/index.tsx
@@ -1,11 +1,13 @@
 import { Banner } from './Banner'
 import { Segments } from '../WebsiteLayout/Segments'
 import { ContactForm } from './ContactForm'
+import { FeaturedFrame } from './FeaturedFrame'
 
 export const SolutionsPage = () => {
   return (
     <>
       <Banner />
+      <FeaturedFrame />
       <Segments />
       <ContactForm />
     </>
