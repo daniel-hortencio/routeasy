e631d451387a652613ae869cefb5beb23d79256d
cheilamelo <cheila.melo@routeasy.com.br>
2023-05-23 09:23:20 -0300

    Ajustes

--- src/components/layouts/HomePage/Solutions/index.tsx
+++ src/components/layouts/HomePage/Solutions/index.tsx
@@ -7,6 +7,7 @@ import { useKeenSlider } from 'keen-slider/react'
 import { useEffect, useState } from 'react'
 import 'keen-slider/keen-slider.min.css'
 import { TextHighlight, Title } from 'components/elements/Texts'
+import S from './styles.module.css'
 
 const CardSoluctions = ({ icon = '', title, onClick, isActive }) => {
   return (
@@ -79,44 +80,51 @@ export const Solutions = () => {
     }
   })
   return (
-    <Section
-      title={
-        <Title>
-          Encontre as soluções ideais para sua{' '}
-          <TextHighlight>operação</TextHighlight>
-        </Title>
-      }
-      subtitle="Lorem ipsum dolor sit amet. Ut sint laboriosam ut sapiente rerum aut assumenda voluptates qui beatae quis id Quis cupiditate. Cum veritatis voluptatem hic dolores fuga eum dolorum tenetur est iusto quis. "
-    >
-      <div className="hidden md:block">
-        <div className="mt-2 flex items-center justify-evenly">
-          <CardSoluctions
-            isActive={tab === 1}
-            onClick={() => setTab(1)}
-            title="Roteirização"
-          />
-          <CardSoluctions
-            isActive={tab === 2}
-            onClick={() => setTab(2)}
-            title="Gestão de entregas"
-          />
-          <CardSoluctions
-            isActive={tab === 3}
-            onClick={() => setTab(3)}
-            title="Automação"
-          />
-          <CardSoluctions
-            isActive={tab === 4}
-            onClick={() => setTab(4)}
-            title="Aplicativo"
-          />
-          <CardSoluctions
-            isActive={tab === 5}
-            onClick={() => setTab(5)}
-            title="API"
-          />
+    <div>
+      <Section
+        title={
+          <Title>
+            Encontre as soluções ideais para sua{' '}
+            <TextHighlight>operação</TextHighlight>
+          </Title>
+        }
+        subtitle="Lorem ipsum dolor sit amet. Ut sint laboriosam ut sapiente rerum aut assumenda voluptates qui beatae quis id Quis cupiditate. Cum veritatis voluptatem hic dolores fuga eum dolorum tenetur est iusto quis. "
+      >
+        {' '}
+      </Section>
+      <Section>
+        <div className="hidden md:block">
+          <div
+            className={`${S.SoluctionsGrid} mt-2 grid items-center justify-evenly`}
+          >
+            <CardSoluctions
+              isActive={tab === 1}
+              onClick={() => setTab(1)}
+              title="Roteirização"
+            />
+            <CardSoluctions
+              isActive={tab === 2}
+              onClick={() => setTab(2)}
+              title="Gestão de entregas"
+            />
+            <CardSoluctions
+              isActive={tab === 3}
+              onClick={() => setTab(3)}
+              title="Automação"
+            />
+            <CardSoluctions
+              isActive={tab === 4}
+              onClick={() => setTab(4)}
+              title="Aplicativo"
+            />
+            <CardSoluctions
+              isActive={tab === 5}
+              onClick={() => setTab(5)}
+              title="API"
+            />
+          </div>
         </div>
-      </div>
+      </Section>
       <div className="lg:hidden">
         <div ref={sliderRef} className="keen-slider ">
           <div className="ml-5 mt-2 flex items-center justify-between">
@@ -158,69 +166,75 @@ export const Solutions = () => {
           </div>
         </div>
       </div>
-      <div className="w-full relative my-12 flex justify-center">
-        {tab === 1 && (
-          <Image
-            src="/images/pictures/Screen roteirizacao.svg"
-            width="842"
-            height="543"
-            alt="Roteirização"
-          />
-        )}
-        {tab === 2 && (
-          <Image
-            src="/images/pictures/Screen gestao.svg"
-            width="842"
-            height="543"
-            alt="Gestão de Entregas"
-          />
-        )}
-        {tab === 3 && (
-          <Image
-            src="/images/pictures/Screen automacao API.svg"
-            width="842"
-            height="543"
-            alt="Automação"
-          />
-        )}
-        {tab === 4 && (
-          <Image
-            src="/images/pictures/Screen aplicativo.svg"
-            width="280"
-            height="544"
-            alt="Aplicativo"
-          />
-        )}
-        {tab === 5 && (
-          <Image
-            src="/images/pictures/Screen automacao API.svg"
-            width="842"
-            height="543"
-            alt="API"
-          />
-        )}
-      </div>
-      <div className="md:flex mb-8 items-center justify-between">
-        <CardRoute
-          icon=""
-          title="Personalização de objetivos de otimização"
-        ></CardRoute>
-        <CardRoute icon="" title="Entregas e coletas na mesma rota"></CardRoute>
-        <CardRoute icon="" title="Resequenciamento automático"></CardRoute>
-        <CardRoute
-          icon=""
-          title="Configuração de restrições operacionais"
-        ></CardRoute>
-      </div>
-      <div className="">
-        <p className="md:max-w-[60%] m-auto text-grayscale-200 text-base font-normal text-center">
-          Com o Routing Studio você pode realizar todo o planajamento de suas
-          rotas, sejam elas D+0, D+1 ou Apenas estudar o melhor planejamento.
-        </p>
-      </div>
-      <div className="md:w-32 m-auto mt-8 mb-20 md:mb-24 md:mt-12">
-        <ButtonPrimary href="/">Saiba mais</ButtonPrimary>
-      </div>
-    </Section>
+      <Section>
+        <div className="w-full relative my-12 flex justify-center">
+          {tab === 1 && (
+            <Image
+              src="/images/pictures/Screen roteirizacao.svg"
+              width="842"
+              height="543"
+              alt="Roteirização"
+            />
+          )}
+          {tab === 2 && (
+            <Image
+              src="/images/pictures/Screen gestao.svg"
+              width="842"
+              height="543"
+              alt="Gestão de Entregas"
+            />
+          )}
+          {tab === 3 && (
+            <Image
+              src="/images/pictures/Screen automacao API.svg"
+              width="842"
+              height="543"
+              alt="Automação"
+            />
+          )}
+          {tab === 4 && (
+            <Image
+              src="/images/pictures/Screen aplicativo.svg"
+              width="280"
+              height="544"
+              alt="Aplicativo"
+            />
+          )}
+          {tab === 5 && (
+            <Image
+              src="/images/pictures/Screen automacao API.svg"
+              width="842"
+              height="543"
+              alt="API"
+            />
+          )}
+        </div>
+
+        <div className="md:flex mb-8 items-center justify-between">
+          <CardRoute
+            icon=""
+            title="Personalização de objetivos de otimização"
+          ></CardRoute>
+          <CardRoute
+            icon=""
+            title="Entregas e coletas na mesma rota"
+          ></CardRoute>
+          <CardRoute icon="" title="Resequenciamento automático"></CardRoute>
+          <CardRoute
+            icon=""
+            title="Configuração de restrições operacionais"
+          ></CardRoute>
+        </div>
+        <div className="">
+          <p className="md:max-w-[60%] m-auto text-grayscale-200 text-base font-normal text-center">
+            Com o Routing Studio você pode realizar todo o planajamento de suas
+            rotas, sejam elas D+0, D+1 ou Apenas estudar o melhor planejamento.
+          </p>
+        </div>
+        <div className="md:w-32 m-auto mt-8 mb-20 md:mb-24 md:mt-12">
+          <ButtonPrimary href="/">Saiba mais</ButtonPrimary>
+        </div>
+      </Section>
+    </div>
   )
 }
