02854ebb02f7661dbefd89da9d57e1ad363ea9d1
cheilamelo <cheila.melo@routeasy.com.br>
2023-05-24 10:37:36 -0300

    Maestro componetized in WebsiteLayout and reused Solutions page

--- src/components/layouts/SolutionsPage/FeaturedFrame/index.tsx
+++ src/components/layouts/SolutionsPage/FeaturedFrame/index.tsx
@@ -9,15 +9,13 @@ interface ITextBoxProps {
 
 export const FeaturedFrame = ({ title, text, image }: ITextBoxProps) => {
   return (
-    <Section>
+    <Section className="max-w-[968px] m-auto">
       <div className="mb-16 md:flex bg-grayscale-500 justify-between items-center m-auto pb-10 md:pb-8 pt-8 md:pt-6 px-12 md:px-16 rounded-2xl">
         <div className="text-center md:text-left w-full md:w-2/4 mb-8 md:mb-0">
-          <h3 className="mb-2 md:mb-3 text-[32px] font-medium uppercase">
-            {title}
-          </h3>
-          <p>{text}</p>
+          <h3 className="mb-2 md:mb-3 text-2xl font-bold uppercase">{title}</h3>
+          <p className="text-sm text-grayscale-50 font-normal">{text}</p>
         </div>
-        <div className="w-full md:w-[383px] h-[243px] relative">
+        <div className="w-full md:w-[316px] h-[198px] relative">
           <Image src={image} fill alt="Screen" />
         </div>
       </div>
