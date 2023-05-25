62f386ef6cb878124119ca68d011c5e9c61c5744
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-05-25 17:28:44 -0400

    adjusts video player

--- src/components/layouts/WebsiteLayout/Maestro/index.tsx
+++ src/components/layouts/WebsiteLayout/Maestro/index.tsx
@@ -3,6 +3,7 @@
 import { Section } from 'components/elements/Section'
 import { ButtonSecondary } from 'components/elements/Button'
 import React from 'react'
+import Image from 'next/image'
 import ReactPlayer from 'react-player/lazy'
 import { TextHighlight, Title } from 'components/elements/Texts'
 
@@ -16,11 +17,24 @@ export const Maestro = () => {
       }
       subtitle="Coloque sua operação no piloto automático enquanto você gerencia apenas as exceções. O Maestro automatiza 100% das tarefas manuais, possibilitando o same day delivery e as coletas dinâmicas graças às regras e parâmetrizações que identificam sempre a melhor rota. Conte ainda com o despacho automático para um motorista disponível mais próximo."
     >
-      <div className="flex justify-center">
+      <div className="flex justify-center w-full pt-[56%] relative">
         <ReactPlayer
-          light={true}
+          width="100%"
+          height="100%"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0
+          }}
+          light={
+            <Image
+              fill
+              src="/images/pictures/video-player-thumbnail.png"
+              alt="Thumbnail"
+            />
+          }
           url="https://www.youtube.com/watch?v=L3WzRR5Ugks&t=1s"
-        ></ReactPlayer>
+        />
       </div>
       <div className="md:w-40 text-sm m-auto my-12 mb-20">
         <ButtonSecondary>Quero saber mais</ButtonSecondary>
