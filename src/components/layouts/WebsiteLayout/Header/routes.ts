3447eeac7f80c279bd767be2de7866d7ae98efcb
daniel-hortencio <hortencio.daniel@gmail.com>
2023-06-16 10:52:36 -0400

    adjusts in route solucoes/routeirizacao

--- src/components/layouts/WebsiteLayout/Header/routes.ts
+++ src/components/layouts/WebsiteLayout/Header/routes.ts
@@ -7,7 +7,7 @@ const routes = (pathname: string) => [
         label: 'Todas soluções',
         href: '/solucoes/'
       },
-      { label: 'Roteirizador', href: '/solucoes/' },
+      { label: 'Roteirizador', href: '/solucoes/routeirizacao' },
       { label: 'Api', href: '/solucoes/api' },
       { label: 'Onboarding', href: '/solucoes/onboarding' }
     ],
