a931f09a092cc049fa7dc83e9a74c64008acfb2e
daniel-hortencio <hortencio.daniel@gmail.com>
2023-06-15 20:34:34 -0400

    adjusts in contact page

--- src/components/layouts/WebsiteLayout/BannerOverlay/index.tsx
+++ src/components/layouts/WebsiteLayout/BannerOverlay/index.tsx
@@ -29,8 +29,15 @@ export const BannerOverlay = ({ image, children }: IBannerOverlay) => {
 
   return (
     <Section className="pb-20 lg:pb-28">
-      <div className="relative overflow-hidden rounded-3xl w-full text-center py-10 md:py-11 pb-16">
-        <Image src={image} fill alt={image} style={{ objectFit: 'cover' }} />
+      <div className="test relative overflow-hidden rounded-3xl w-full text-center py-10 md:py-11 pb-16">
+        <Image
+          src={image}
+          fill
+          alt={image}
+          style={{
+            objectFit: 'cover'
+          }}
+        />
         <div className="relative mx-auto w-16 h-16 mb-8 md:mb-10">
           <Image src="/images/logos/union.svg" fill alt="Recicável" />
         </div>
