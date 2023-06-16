239ce91e3537e0f27993e352c77a69f6596b4e38
daniel-hortencio <hortencio.daniel@gmail.com>
2023-06-15 20:05:50 -0400

    adjusts in section Testimonials in home

--- src/components/layouts/WebsiteLayout/Testimonials/index.tsx
+++ src/components/layouts/WebsiteLayout/Testimonials/index.tsx
@@ -6,7 +6,7 @@ import { SlideTestimonials } from './Slide'
 export const Testimonials = () => {
   return (
     <Section
-      className="pb-20 lg:pb-28"
+      className="pb-20"
       title={
         <Title>
           Quem usa <TextHighlight>confia</TextHighlight>
@@ -14,7 +14,7 @@ export const Testimonials = () => {
       }
       subtitle="Lorem ipsum dolor sit amet. Ut sint laboriosam ut sapiente rerum aut assumenda voluptates qui beatae quis id Quis cupiditate. Cum veritatis voluptatem hic dolores fuga eum dolorum tenetur est iusto quis. "
     >
-      <div className="mb-[88px] lg:mb-32">
+      <div>
         <div className="mb-12 ">
           <SlideTestimonials />
         </div>
