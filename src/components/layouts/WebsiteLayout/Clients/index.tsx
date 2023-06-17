57d825a7ba13e815902b28b0ec7b400c6aa7abf7
cheilamelo <cheila.melo@routeasy.com.br>
2023-06-16 21:06:35 -0300

    Adjust component clients

--- src/components/layouts/WebsiteLayout/Clients/index.tsx
+++ src/components/layouts/WebsiteLayout/Clients/index.tsx
@@ -5,17 +5,37 @@ import { Section } from 'components/elements/Section'
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
+const animation = { duration: 5000, easing: (t: number) => t }
+
 export const Clients = () => {
   const [sliderRef] = useKeenSlider({
-    initial: 0,
-    slides: {
-      perView: 2.5,
-      spacing: 20
+    loop: true,
+    renderMode: 'performance',
+    drag: false,
+    created(s) {
+      s.moveToIdx(5, true, animation)
+    },
+    updated(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
+    },
+    animationEnded(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation)
     },
     breakpoints: {
-      '(min-width: 640px)': {
+      '(max-width: 639px)': {
         slides: {
-          perView: 3.25
+          perView: 3
+        }
+      },
+      '(min-width: 1024px)': {
+        slides: {
+          perView: 7,
+          spacing: 15
+        }
+      },
+      '(min-width: 640px) and (max-width: 1023px)': {
+        slides: {
+          perView: 4
         }
       }
     }
@@ -26,8 +46,13 @@ export const Clients = () => {
     { src: '/images/logos/solist.png', alt: 'Solistica' },
     { src: '/images/logos/shop.png', alt: 'Shopee' },
     { src: '/images/logos/mag.png', alt: 'Magalu' },
-    { src: '/images/logos/mag.png', alt: 'Magalu' },
-    { src: '/images/logos/mag.png', alt: 'Magalu' }
+    { src: '/images/logos/ultra.png', alt: 'Ultra Gaz' },
+    { src: '/images/logos/ML.png', alt: 'Mercado Livre' },
+    { src: '/images/logos/gpa.png', alt: 'GPA' },
+    { src: '/images/logos/rte.png', alt: 'Rodonaves' },
+    { src: '/images/logos/royal.png', alt: 'Royal Canin' },
+    { src: '/images/logos/ze.png', alt: 'Zé Delivery' },
+    { src: '/images/logos/oggi.png', alt: 'Oggi' }
   ]
 
   return (
@@ -35,15 +60,7 @@ export const Clients = () => {
       <div className="hidden md:block mx-auto w-[calc(100%-3rem)] h-[6px] rounded-t-lg bg-grayscale-600" />
 
       <div className="mx-auto bg-grayscale-500 rounded-lg shadow-3xl">
-        <div className="hidden md:flex justify-evenly">
-          {clients.map((client, index) => (
-            <div className="w-[100px] h-[100px] relative" key={index}>
-              <Image src={client.src} fill alt={client.alt} />
-            </div>
-          ))}
-        </div>
-
-        <div className="md:hidden py-5">
+        <div className="py-5">
           <div ref={sliderRef} className="keen-slider ">
             {clients.map((client, index) => (
               <div
