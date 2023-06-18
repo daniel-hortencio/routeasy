11708ef4ca391de9e5346ec862444778b1f0a052
daniel-hortencio <hortencio.daniel@gmail.com>
2023-06-18 14:39:16 -0400

    adjusts in slide for about

--- src/app/(rootTemplate)/sobre-nos/History/index.tsx
+++ src/app/(rootTemplate)/sobre-nos/History/index.tsx
@@ -15,8 +15,7 @@ export const History = () => {
   const [sliderRef, instanceRef] = useKeenSlider({
     initial: 0,
     slides: {
-      perView: 1.25,
-      spacing: 20
+      perView: 1.25
     },
     breakpoints: {
       '(min-width: 640px)': {
@@ -42,14 +41,14 @@ export const History = () => {
 
   const CardHistory = ({ title, text }) => {
     return (
-      <div>
+      <div className="h-full flex flex-col">
         <div className="mb-8 flex items-center">
           <p className="text-2xl font-semibold pr-6">{title}</p>
           <div className="w-full border-t border-dashed border-bluescale-100"></div>
         </div>
         <Dot />
-        <div className="pl-6 lg:pl-[1.7rem]">
-          <p className="h-[287px] lg:h-36 pl-0 lg:pl-4 pb-4 lg:pb-0 border-b lg:border-b-0 border-l-0 lg:border-l border-[#424678]">
+        <div className="ml-6 lg:ml-[1.7rem] flex-auto border-b lg:border-b-0 border-[#424678]">
+          <p className="lg:h-36 pl-0 lg:pl-4 pb-4 lg:pb-0 border-l-0 lg:border-l border-[#424678]">
             {text}
           </p>
         </div>
@@ -58,58 +57,63 @@ export const History = () => {
   }
 
   return (
-    <Section
-      className="pb-12 lg:pb-20 bg-grayscale-700 pt-16"
-      title={<Title>Nossa história</Title>}
-      subtitle={
-        <Text className="text-grayscale-50 text-[15px] font-light">
-          Nossa história é moldada inovação contínua e uma mentalidade voltada
-          para o futuro. Estamos animados para continuar a crescer, levando mais
-          tecnologia para o setor de logística.
-        </Text>
-      }
-    >
-      <ScrollElement name="our-history">
-        <div className="grid grid-cols-2 gap-4 w-28 lg:w-20 mx-auto pb-16">
-          {loaded ? (
-            <button
-              className="bg-white rounded-full w-12 lg:w-8 h-12 lg:h-8 flex items-center justify-center fill-grayscale-300 disabled:opacity-50"
-              onClick={() => instanceRef.current?.prev()}
-            >
-              <Icon name="FiArrowLeft" size={16} />
-            </button>
-          ) : (
-            <Skeleton className="w-12 lg:w-8 h-12 lg:h-8 rounded-full" />
-          )}
-          {loaded ? (
-            <button
-              className="bg-white rounded-full w-12 lg:w-8 h-12 lg:h-8 flex items-center justify-center fill-grayscale-300 disabled:opacity-50"
-              onClick={() => instanceRef.current?.next()}
-            >
-              <Icon name="FiArrowRight" size={16} />
-            </button>
-          ) : (
-            <Skeleton className="w-12 lg:w-8 h-12 lg:h-8 rounded-full" />
-          )}
-        </div>
-      </ScrollElement>
+    <>
+      <Section
+        className="pb-12 lg:pb-20 bg-grayscale-700 pt-16"
+        title={<Title>Nossa história</Title>}
+        subtitle={
+          <Text className="text-grayscale-50 text-[15px] font-light">
+            Nossa história é moldada inovação contínua e uma mentalidade voltada
+            para o futuro. Estamos animados para continuar a crescer, levando
+            mais tecnologia para o setor de logística.
+          </Text>
+        }
+      >
+        <ScrollElement name="our-history">
+          <div className="grid grid-cols-2 gap-4 w-28 lg:w-20 mx-auto pb-16">
+            {loaded ? (
+              <button
+                className="bg-white rounded-full w-12 lg:w-8 h-12 lg:h-8 flex items-center justify-center fill-grayscale-300 disabled:opacity-50"
+                onClick={() => instanceRef.current?.prev()}
+              >
+                <Icon name="FiArrowLeft" size={16} />
+              </button>
+            ) : (
+              <Skeleton className="w-12 lg:w-8 h-12 lg:h-8 rounded-full" />
+            )}
+            {loaded ? (
+              <button
+                className="bg-white rounded-full w-12 lg:w-8 h-12 lg:h-8 flex items-center justify-center fill-grayscale-300 disabled:opacity-50"
+                onClick={() => instanceRef.current?.next()}
+              >
+                <Icon name="FiArrowRight" size={16} />
+              </button>
+            ) : (
+              <Skeleton className="w-12 lg:w-8 h-12 lg:h-8 rounded-full" />
+            )}
+          </div>
+        </ScrollElement>
+      </Section>
       {loaded ? (
-        <div ref={sliderRef} className="keen-slider mb-12">
-          <div className="keen-slider__slide pr-6">
+        <div
+          ref={sliderRef}
+          className="keen-slider mb-12 w-full max-w-6xl mx-auto flex"
+        >
+          <div className="keen-slider__slide pl-6">
             <CardHistory
               title="2016"
               text={
                 <Text className="text-grayscale-50 text-[15px] font-light">
                   Durantesssss seu projeto de mestrado, nosso CEO, Caio Reina,
                   criou um algoritmo de roteirização capaz de superar resultados
-                  de soluções já estabelecidas no mercado. Após vários testes em
+                  de soluções já estabelecidas no mercado. Após várioses em
                   operações reais, o algoritmo foi integrado em uma plataforma
                   web e, assim, nascia a RoutEasy.
                 </Text>
               }
             />
           </div>
-          <div className="keen-slider__slide pr-6">
+          <div className="keen-slider__slide pl-6">
             <CardHistory
               title="2017"
               text={
@@ -122,7 +126,7 @@ export const History = () => {
               }
             />
           </div>
-          <div className="keen-slider__slide pr-6">
+          <div className="keen-slider__slide pl-6">
             <CardHistory
               title="2019"
               text={
@@ -136,7 +140,7 @@ export const History = () => {
               }
             />
           </div>
-          <div className="keen-slider__slide pr-6">
+          <div className="keen-slider__slide pl-6">
             <CardHistory
               title="2020"
               text={
@@ -149,7 +153,7 @@ export const History = () => {
               }
             />
           </div>
-          <div className="keen-slider__slide pr-6">
+          <div className="keen-slider__slide pl-6">
             <CardHistory
               title="2021"
               text={
@@ -162,7 +166,7 @@ export const History = () => {
               }
             />
           </div>
-          <div className="keen-slider__slide pr-6">
+          <div className="keen-slider__slide pl-6">
             <CardHistory
               title="2022"
               text={
@@ -177,7 +181,7 @@ export const History = () => {
               }
             />
           </div>
-          <div className="keen-slider__slide pr-6">
+          <div className="keen-slider__slide pl-6 pr-6">
             <CardHistory
               title="2023"
               text={
@@ -191,8 +195,8 @@ export const History = () => {
           </div>
         </div>
       ) : (
-        <Skeleton className="w-full h-[260px] rounded-lg" />
+        <Skeleton className="w-full h-[260px] rounded-lg max-w-6xl mx-auto" />
       )}
-    </Section>
+    </>
   )
 }
