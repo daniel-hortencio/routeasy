ed6fd349c8eb3bbfcdeb36ec3348701fef45836c
daniel-hortencio <hortencio.daniel@gmail.com>
2023-06-18 12:28:15 -0400

    feat: validation corporative email

--- src/components/layouts/WebsiteLayout/ContactForm/index.tsx
+++ src/components/layouts/WebsiteLayout/ContactForm/index.tsx
@@ -64,7 +64,34 @@ export const ContactForm = () => {
       email: yup
         .string()
         .required('Obrigatório')
-        .email('Insira um e-mail com formato válido'),
+        .email('Insira um e-mail com formato válido')
+        .test('is-corporate-email', 'Deve ser um e-mail corporativo', email => {
+          if (!email) return true // O valor está vazio, a validação é ignorada
+
+          const [_, e_mail_domain] = email.split('@')
+
+          const not_corporative_email_domains = [
+            'gmail.com',
+            'yahoo.com',
+            'outlook.com',
+            'hotmail.com',
+            'icloud.com',
+            'aol.com',
+            'mail.com',
+            'protonmail.com',
+            'zoho.com',
+            'yandex.com',
+            'gmx.com',
+            'outlook.com.br',
+            'live.com',
+            'rediffmail.com',
+            'mail.ru'
+          ]
+          if (not_corporative_email_domains.includes(e_mail_domain))
+            return false // Não é um e-mail corporativo válido
+
+          return true // Não é um e-mail corporativo válido
+        }),
       personal_phone: yup
         .string()
         .required('Obrigatório')
