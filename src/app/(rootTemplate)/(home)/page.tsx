11883615792700989e1eeaa112bda1803abdc06f
daniel-hortencio <hortencio.daniel@gmail.com>
2023-06-23 12:11:41 -0400

    hidden section solutions in home

--- src/app/(rootTemplate)/(home)/page.tsx
+++ src/app/(rootTemplate)/(home)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <>
       <Banner />
       <Clients />
-      <Solutions />
+      {/*       <Solutions /> */}
       <Integrating />
       <RoutingAcademy />
       <Maestro />
