0e44f6d54030fb453b507b5da4cdc878f58b02c8
daniel-hortencio <hortencio.daniel@gmail.com>
2023-06-23 13:22:35 -0400

    adjusts banner about us mobile

--- src/app/(rootTemplate)/sobre-nos/Banner/index.tsx
+++ src/app/(rootTemplate)/sobre-nos/Banner/index.tsx
@@ -6,52 +6,50 @@ import { Link as LinkScroll } from 'react-scroll'
 
 export const Banner = () => {
   return (
-    <>
-      <div className="relative w-full h-full bg-grayscale-700">
-        <Image
-          style={{
-            objectFit: 'cover'
-          }}
-          fill
-          src="/images/pictures/banner-sobre-nos.png"
-          alt="Banner"
-        />
-        <div className="flex justify-center items-center h-full relative min-h-[600px]">
-          <div className="pt-16 md:pt-56 md:mx-auto lg:mx-0 flex flex-col justify-center w-[645px] text-center">
-            <Title className="text-white uppercase font-medium text-5xl lg:text-[56px]/[72px] mb-5">
-              Sobre
-              <TextHighlight> nós</TextHighlight>
-            </Title>
-            <Text className="text-grayscale-50 text-[15px] font-light mb-2">
-              Somos mais de 100 mentes inovadoras compartilhando a missão de
-              revolucionar a logística no Brasil.
-            </Text>
-            <Text className="text-grayscale-50 text-[15px] font-light">
-              Acreditamos que nossos talentos são o que nos diferencia. Cada
-              membro da nossa equipe tem conhecimentos e habilidades únicas, que
-              se combinam para criar uma cultura única e poderosa.
-            </Text>
-            <div className="hidden md:flex justify-center pt-20">
-              <LinkScroll
-                smooth={true}
-                to="our-history"
-                offset={-320}
-                duration={400}
-              >
-                <Image
-                  width={40}
-                  height={100}
-                  alt="Scroll"
-                  src="/images/icons/button-scroll.svg"
-                  style={{
-                    objectFit: 'cover'
-                  }}
-                />
-              </LinkScroll>
-            </div>
+    <div className="relative w-full h-screen md:h-full bg-grayscale-700">
+      <Image
+        style={{
+          objectFit: 'cover'
+        }}
+        fill
+        src="/images/pictures/banner-sobre-nos.png"
+        alt="Banner"
+      />
+      <div className="flex justify-center items-center h-full relative min-h-[600px]">
+        <div className="pt-24 md:pt-56 md:mx-auto lg:mx-0 flex flex-col justify-center w-[645px] text-center">
+          <Title className="text-white uppercase font-medium text-5xl lg:text-[56px]/[72px] mb-5 px-5">
+            Sobre aqui
+            <TextHighlight> nós</TextHighlight>
+          </Title>
+          <Text className="text-grayscale-50 text-[15px] font-light mb-2 px-5">
+            Somos mais de 100 mentes inovadoras compartilhando a missão de
+            revolucionar a logística no Brasil.
+          </Text>
+          <Text className="text-grayscale-50 text-[15px] font-light px-5">
+            Acreditamos que nossos talentos são o que nos diferencia. Cada
+            membro da nossa equipe tem conhecimentos e habilidades únicas, que
+            se combinam para criar uma cultura única e poderosa.
+          </Text>
+          <div className="flex justify-center pt-36 md:pt-20">
+            <LinkScroll
+              smooth={true}
+              to="our-history"
+              offset={-320}
+              duration={400}
+            >
+              <Image
+                width={40}
+                height={100}
+                alt="Scroll"
+                src="/images/icons/button-scroll.svg"
+                style={{
+                  objectFit: 'cover'
+                }}
+              />
+            </LinkScroll>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
