84adaa00d682f66d57358b46314049e212b8bded
cheilamelo <cheila.melo@routeasy.com.br>
2023-06-27 14:54:35 -0300

    Adjust component Banner home page

--- src/app/(rootTemplate)/(home)/Banner/index.tsx
+++ src/app/(rootTemplate)/(home)/Banner/index.tsx
@@ -29,9 +29,9 @@ export const Banner = () => {
     <Section className="flex lg:mb-12">
       <div className="flex h-full relative min-h-[640px]">
         <div className="md:max-w-[32rem] pt-20 md:mx-auto lg:mx-0 flex flex-col justify-center w-full h-full text-center lg:text-left">
-          <h1 className="text-white uppercase font-semibold text-5xl lg:text-[50px]/[68px] pb-5">
+          <h1 className="text-white uppercase font-semibold text-5xl lg:text-[47px]/[68px] pb-5">
             Tecnologia avançada para
-            <div className="flex justify-center lg:justify-start">
+            <div className="flex justify-center lg:justify-start mb-12 lg:mb-0">
               <TextHighlight>
                 <TextTransition springConfig={presets.wobbly}>
                   {list_operations[index % list_operations.length]}
